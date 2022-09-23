9b23eef821ebeffd679e20ef479b612fd02455f4
kkk125504 <kginyc5504@gmail.com>
2022-09-23 22:08:51 +0900

    count 컨트롤 액션

--- src/main/java/com/kjh/exam/demo/controller/UsrHomeController.java
+++ src/main/java/com/kjh/exam/demo/controller/UsrHomeController.java
@@ -6,34 +6,18 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class UsrHomeController {
-	int count = 0;
-	@RequestMapping("/usr/home/main")
+	private int count = 0;
+	
+	@RequestMapping("/usr/home/getCount")
 	@ResponseBody
-	public String showMain() {
-		return "안녕하세요";
+	public int getCount() {		
+		return count;
 	}
 	
-	@RequestMapping("/usr/home/main2")
+	@RequestMapping("/usr/home/doSetCount")
 	@ResponseBody
-	public String showMain2() {
-		return "반갑습니다";
-	}
-	
-	@RequestMapping("/usr/home/main3")
-	@ResponseBody
-	public String showMain3() {
-		return "잘가요";
-	}
-	@RequestMapping("/usr/home/main4")
-	@ResponseBody
-	public int showMain4() {
-		return count++;
-	}
-	
-	@RequestMapping("/usr/home/main5")
-	@ResponseBody
-	public String showMain5() {
-		this.count = 0;
-		return "Count의 값이"+ this.count +"로 초기화 됬습니다.";
+	public String doSetCount(int count) {
+		this.count = count;
+		return "Count의 값이"+ count +"로 초기화 됬습니다.";
 	}
 }
