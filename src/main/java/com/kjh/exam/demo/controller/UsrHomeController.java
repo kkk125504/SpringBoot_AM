b13907dd87b0e3d407b69e381538ed7b162f73d7
kkk125504 <kginyc5504@gmail.com>
2022-09-23 21:48:21 +0900

    usr/home/main2, main3추가

--- src/main/java/com/kjh/exam/demo/controller/UsrHomeController.java
+++ src/main/java/com/kjh/exam/demo/controller/UsrHomeController.java
@@ -6,9 +6,21 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class UsrHomeController {
-	@RequestMapping("/usr/main/home")
+	@RequestMapping("/usr/home/main")
 	@ResponseBody
 	public String showMain() {
-		return "ㅎㅇ";
+		return "안녕하세요";
+	}
+	
+	@RequestMapping("/usr/home/main2")
+	@ResponseBody
+	public String showMain2() {
+		return "반갑습니다";
+	}
+	
+	@RequestMapping("/usr/home/main3")
+	@ResponseBody
+	public String showMain3() {
+		return "잘가요";
 	}
 }
