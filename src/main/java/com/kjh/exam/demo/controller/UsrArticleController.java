6c330fd8c3872294737881513e38ae6298a4a0b8
kkk125504 <kginyc5504@gmail.com>
2022-09-23 23:00:04 +0900

    ArticleController 게시물추가, 리스트 구현

---
+++ src/main/java/com/kjh/exam/demo/controller/UsrArticleController.java
@@ -0,0 +1,39 @@
+package com.kjh.exam.demo.controller;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.kjh.exam.demo.vo.Article;
+
+@Controller
+public class UsrArticleController {
+	public List<Article> articles;
+	private int lastArticleId;
+	public UsrArticleController() {
+		articles = new ArrayList<>();
+		lastArticleId =0;
+	}
+		
+	@RequestMapping("usr/article/doAdd")
+	@ResponseBody
+	public String doAdd(String title, String body) {
+		int id = lastArticleId + 1;
+		lastArticleId = id;
+		Article article = new Article(id, title, body);
+		articles.add(article);
+		return article+"이 추가되었습니다.";
+	}
+
+	@RequestMapping("usr/article/getArticles")
+	@ResponseBody
+	public List<Article> getArticles() {
+		
+		return articles;
+	}
+}
