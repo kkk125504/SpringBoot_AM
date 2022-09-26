ee856bf1138feee7a0073c6333061416ba5239f4
kkk125504 <kginyc5504@gmail.com>
2022-09-26 20:34:52 +0900

    게시물 테스트 데이터 생성

--- src/main/java/com/kjh/exam/demo/controller/UsrArticleController.java
+++ src/main/java/com/kjh/exam/demo/controller/UsrArticleController.java
@@ -15,12 +15,27 @@ import com.kjh.exam.demo.vo.Article;
 public class UsrArticleController {
 	public List<Article> articles;
 	private int lastArticleId;
-	
+
 	public UsrArticleController() {
 		articles = new ArrayList<>();
-		lastArticleId =0;
+		lastArticleId = 0;
+		makeTestData();
 	}
-		
+
+	// 서비스 메소드
+	private void makeTestData() {
+		for (int i = 1; i <= 10; i++) {
+			int id = lastArticleId + 1;
+			String title = "제목" + i;
+			String body = "내용" + i;
+			Article article = new Article(id, title, body);
+			articles.add(article);
+			lastArticleId = id;
+		}
+
+	}
+
+	// 액션 메소드
 	@RequestMapping("usr/article/doAdd")
 	@ResponseBody
 	public String doAdd(String title, String body) {
@@ -28,13 +43,13 @@ public class UsrArticleController {
 		lastArticleId = id;
 		Article article = new Article(id, title, body);
 		articles.add(article);
-		return article+"이 추가되었습니다.";
+		return article + "이 추가되었습니다.";
 	}
 
 	@RequestMapping("usr/article/getArticles")
 	@ResponseBody
 	public List<Article> getArticles() {
-		
+
 		return articles;
 	}
 }
