fa50a02f04c63440daa9435a9a8688f396cfe871
kkk125504 <kginyc5504@gmail.com>
2022-09-28 22:13:29 +0900

    DB스키마 추가

---
+++ DB.sql
@@ -0,0 +1,83 @@
+#DB 생성
+DROP DATABASE IF EXISTS SB_AM;
+CREATE DATABASE SB_AM;
+USE SB_AM;
+# 게시물 테이블 생성
+CREATE TABLE article(
+    id INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    regDate DATETIME NOT NULL,
+    updateDate DATETIME NOT NULL,
+    title CHAR(100) NOT NULL,
+    `body` TEXT NOT NULL
+);
+# 게시물 테스트데이터 생성
+INSERT INTO article 
+SET regDate = NOW(),
+updateDate = NOW(),
+title = '제목 1',
+`body` = '내용 1';
+INSERT INTO article 
+SET regDate = NOW(),
+updateDate = NOW(),
+title = '제목 2',
+`body` = '내용 2';
+INSERT INTO article 
+SET regDate = NOW(),
+updateDate = NOW(),
+title = '제목 3',
+`body` = '내용 3';
+
+# 회원 테이블 생성
+CREATE TABLE `member`(
+    id INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    regDate DATETIME NOT NULL,
+    updateDate DATETIME NOT NULL,
+    loginId CHAR(20) NOT NULL,
+    loginPw CHAR(60) NOT NULL,
+    `authLevel` SMALLINT(2) UNSIGNED DEFAULT 3 COMMENT '권한 레벨 (3=일반,7=관리자)',
+    `name` CHAR(20) NOT NULL, 
+    nickname CHAR(20) NOT NULL,
+    cellphoneNum CHAR(20) NOT NULL,
+    email CHAR(50) NOT NULL,
+    delStatus TINYINT(1) UNSIGNED NOT NULL DEFAULT 0 COMMENT '탈퇴여부 (0=탈퇴 전,1=탈퇴 후)',
+    delDate DATETIME COMMENT '탈퇴날짜'
+);
+
+# 회원 테스트데이터 생성 (관리자)
+INSERT INTO `member` 
+SET regDate = NOW(),
+updateDate = NOW(),
+loginId = 'admin',
+loginPw = 'admin',
+`authLevel` = 7,
+`name` = '관리자', 
+nickname = '관리자',
+cellphoneNum = '01012341234',
+email = 'kginyc5504@gmail.com';
+
+# 회원 테스트데이터 생성 (일반)
+INSERT INTO `member` 
+SET regDate = NOW(),
+updateDate = NOW(),
+loginId = 'test1',
+loginPw = 'test1',
+`name` = '사용자1', 
+nickname = '사용자1',
+cellphoneNum = '01043214321',
+email = 'kginyc5504@gmail.com';
+
+INSERT INTO `member` 
+SET regDate = NOW(),
+updateDate = NOW(),
+loginId = 'test2',
+loginPw = 'test2',
+`name` = '사용자2', 
+nickname = '사용자2',
+cellphoneNum = '01067896789',
+email = 'kginyc5504@gmail.com';
+
+SELECT * FROM `member`;
+
+SELECT * FROM article;
+
+SELECT LAST_INSERT_ID();
