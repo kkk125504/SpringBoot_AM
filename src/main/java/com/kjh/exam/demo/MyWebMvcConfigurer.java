909827111cc110a044be3563b54f8c0bec1b7fbb
kkk125504 <kginyc5504@gmail.com>
2022-10-11 22:02:04 +0900

    BeforeActionInterceptor registry에 등록

---
+++ src/main/java/com/kjh/exam/demo/MyWebMvcConfigurer.java
@@ -0,0 +1,27 @@
+package com.kjh.exam.demo;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.kjh.exam.demo.interceptor.BeforeActionInterceptor;
+
+@Configuration
+public class MyWebMvcConfigurer implements WebMvcConfigurer {
+	
+	@Autowired
+	BeforeActionInterceptor beforeActionInterceptor;
+
+	// 인터셉터 적용
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(beforeActionInterceptor).addPathPatterns("/**").excludePathPatterns("/resource/**")
+				.excludePathPatterns("/error");
+	}
+
+}
--- src/main/java/com/kjh/exam/demo/interceptor/BeforeActionInterceptor.java
+++ src/main/java/com/kjh/exam/demo/interceptor/BeforeActionInterceptor.java
@@ -5,12 +5,14 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
+
 @Component
 public class BeforeActionInterceptor implements HandlerInterceptor {
-	
+
 	@Override
 	public boolean preHandle(HttpServletRequest req, HttpServletResponse resp, Object handler) throws Exception {
 		System.out.println("실행됨?");
 		return HandlerInterceptor.super.preHandle(req, resp, handler);
 	}
+
 }
